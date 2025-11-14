9c4922bb373e0e51cd14730f094a55f14ebc07c1
Simon Gilli <219259919+btc-simongilli@users.noreply.github.com>
2025-11-14 10:27:54 +0100

    feature install and configure zabbix agent for SmartMonitoring

--- 3cx/post-install.sh
+++ 3cx/post-install.sh
@@ -2,8 +2,26 @@
 
 # Post-installation script for 3CX on Debian
 
-# Install and configure chrony for time synchronization
+# Install Zabbix repository
+wget https://repo.zabbix.com/zabbix/7.4/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.4+debian12_all.deb
+dpkg -i zabbix-release_latest_7.4+debian12_all.deb
 apt update
+
+# Clean up
+rm zabbix-release_latest_7.4+debian12_all.deb
+
+# Install Zabbix agent 2
+apt install zabbix-agent2 -y
+
+# Configure Zabbix agent 2
+echo -e 'Server=192.168.72.5\nServerActive=192.168.72.5\nHostname=' > /etc/zabbix/zabbix_agent2.d/smart_monitoring.conf
+
+# Enable and start Zabbix agent 2 service
+systemctl stop zabbix-agent2
+systemctl enable zabbix-agent2
+systemctl start zabbix-agent2
+
+# Install chrony for time synchronization
 apt install chrony -y
 
 # Configure chrony to use Swiss NTP servers
@@ -11,3 +29,6 @@ echo -e 'pool 0.ch.pool.ntp.org iburst\npool 1.ch.pool.ntp.org iburst\npool 2.ch
 
 # Reload sources to apply changes
 chronyc reload sources
+
+# Final message
+echo "Post-installation script completed."
